f668d003b470c10a34469502f4d31fc75f8d5264
enthusiastic-history-5054 <jason.calvo7@gmail.com>
2020-01-18 06:30:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
-function dwarfRollCall(dwarves) {
+
+
+//first I named and created the arrays to be used in the functions belowg
+//var dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"]
+//var planeteerCalls = ["earth", "wind", "fire", "water", "heart"]
+//var shortWords = ["wind", "fire"]
+//only to later realize this was not necessary
+
+
+function dwarfRollCall(dwarves) { 
+  let listDwarves = [] 
+  for(let i = 0; i < dwarves.length; i++){
+   
+   listDwarves.push(`${i+1}. ${dwarves[i]} `)
+  
+  }
+  return listDwarves.join("");//The join() method joins all elements of an array (or an array-like object) into a string and returns this string.
 }
 
+
 function summonCaptainPlanet(planeteerCalls){
+  let upperCasePlaneteerCalls = []
+  for (let i = 0; i < planeteerCalls.length; i++) {
+    let exclamation = "!"
+    upperCasePlaneteerCalls.push(`${planeteerCalls[i]}${exclamation}`.toUpperCase())
+  }
+  return upperCasePlaneteerCalls
 }
 
 function longPlaneteerCalls(words) {
